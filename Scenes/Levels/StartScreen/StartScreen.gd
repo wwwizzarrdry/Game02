f520392e53b323ca8dadba6ce292391e29d6b8c6
wwwizzarrdry <wwwizzarrdry@yahoo.com>
2024-02-26 19:56:03 -0600

    UI modifications
    added color backgrounds to gamepad selection

--- Scenes/Levels/StartScreen/StartScreen.gd
+++ Scenes/Levels/StartScreen/StartScreen.gd
@@ -1,9 +1,11 @@
 extends Control
 
-@onready var player_1: VBoxContainer = $PanelContainer/MarginContainer/HBoxContainer/Player1
-@onready var player_2: VBoxContainer = $PanelContainer/MarginContainer/HBoxContainer/Player2
-@onready var p1_ready_btn: Button = $PanelContainer/MarginContainer/HBoxContainer/Player1/Button
-@onready var p2_ready_btn: Button = $PanelContainer/MarginContainer/HBoxContainer/Player1/Button
+@onready var player_1: VBoxContainer = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player1
+@onready var player_2: VBoxContainer = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player2
+@onready var p1_ready_btn: Button = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player1/Button
+@onready var p2_ready_btn: Button = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player2/Button
+@onready var p1_texture_rect: TextureRect = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player1/TextureRect
+@onready var p2_texture_rect: TextureRect = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Player2/TextureRect2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,14 +18,20 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton:
-		if event.device == 0:
-			player_1.show()
-			p1_ready_btn.disabled = false
-			p1_ready_btn.text = "READY!"
-			p1_ready_btn.grab_focus()
-
-		if event.device == 1:
-			player_2.show()
-			p2_ready_btn.disabled = false
-			p2_ready_btn.text = "READY!"
-			p2_ready_btn.grab_focus()
+		match event.device:
+			0:
+				player_1.show()
+				p1_ready_btn.disabled = false
+				p1_ready_btn.text = "READY!"
+				p1_ready_btn.grab_focus()
+			1:
+				player_2.show()
+				p2_ready_btn.disabled = false
+				p2_ready_btn.text = "READY!"
+				p2_ready_btn.grab_focus()
+			2:
+				return
+			3:
+				return
+			_:
+				return
